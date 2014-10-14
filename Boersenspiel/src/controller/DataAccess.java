ca29fb3e66225b8197fcca1935e27d30167cb0b0
MDiemon <m.diemon@web.de>
2014-10-14 20:55:46 +0200

    EndOfDay.java, StockExchange.java, DataAccess.java Repair start of Project

---
+++ Boersenspiel/src/controller/DataAccess.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.util.ArrayList;
+
+public class DataAccess {
+
+	public static ArrayList<String> getStocks() //TODO - ArrayList<Stock>
+	{
+		//TODO
+		//return model.getStocks();
+		
+		ArrayList<String> ret = new ArrayList<String>();
+		ret.add("Test");
+		return ret;
+	}
+}
---
+++ Boersenspiel/src/controller/StockExchange.java
@@ -0,0 +1,18 @@
+package controller;
+
+public class StockExchange {
+
+	public static void sellStock()//(Stock stock, Cost cst) oder ID
+	{
+		//TODO
+		//Model.buyStock(User.getID, Stock.getID, cst);
+		//User.setBalance(User.getBalance - cst);
+	}
+	
+	public static void buyStock()//(Stock stock, Cost cst) oder ID
+	{
+		//TODO
+		//Model.sellStock(User.getID, Stock.getID, cst);
+		//User.setBalance(User.getBalance + cst);
+	}
+}
