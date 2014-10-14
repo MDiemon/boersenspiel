fbd7a6d71c340ba668d22491d103befb9e33b631
mahlbrand <mahlbrand@web.de>
2014-10-14 19:01:41 +0200

    Class DatabaseConnection, Stock and User created

---
+++ Boersenspiel/src/model/DatabaseConnection.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.List;
+
+import javax.management.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DatabaseConnection {
+	
+	private EntityManagerFactory emf = 
+			Persistence.createEntityManagerFactory("h2");
+
+	private static DatabaseConnection instance = null;
+	
+	public static DatabaseConnection getInstance(){
+		if(null == instance){
+			instance = new DatabaseConnection();
+		}
+		return instance;
+	}
+	
+	/*public void save(GuestbookEntry gbe){
+		EntityManager em = emf.createEntityManager();
+		gbe.setCreated(new Date());
+		em.getTransaction().begin();
+		em.persist(gbe);
+		em.getTransaction().commit();
+	}
+	
+		public List<GuestbookEntry> getAll(){
+		String queryString = "SELECT gbe FROM GuestbookEntry gbe ORDER BY gbe.created DESC";
+		EntityManager em = emf.createEntityManager();
+		Query query = em.createQuery(queryString);
+		return query.getResultList();
+	}*/
+	
+
+	
+	public List<User> getBalance()
+	{
+		String queryString = "";
+		EntityManager em = emf.createEntityManager();
+		Query query = em.createQuery(queryString);
+		return query.getResultList();
+	}
+}
