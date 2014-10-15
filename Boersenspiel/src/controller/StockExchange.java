eaf96409139de404d93a6c8b7d4756dcc3d16339
MDiemon <m.diemon@web.de>
2014-10-15 16:45:09 +0200

    id

--- Boersenspiel/src/controller/StockExchange.java
+++ Boersenspiel/src/controller/StockExchange.java
@@ -2,14 +2,14 @@ package controller;
 
 public class StockExchange {
 
-	public static void sellStock()//(Stock stock, Cost cst) oder ID
+	public static void sellStock(int id)//(Stock stock, Cost cst) oder ID
 	{
 		//TODO
 		//Model.buyStock(User.getID, Stock.getID, cst);
 		//User.setBalance(User.getBalance - cst);
 	}
 	
-	public static void buyStock()//(Stock stock, Cost cst) oder ID
+	public static void buyStock(int id)//(Stock stock, Cost cst) oder ID
 	{
 		//TODO
 		//Model.sellStock(User.getID, Stock.getID, cst);
