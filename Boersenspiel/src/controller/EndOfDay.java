e1c0a453b11fb5bd2fedf8a6598bfec83d9f31e7
marcbode <marc.bode.mail@googlemail.com>
2014-10-15 13:52:51 +0200

    Änderung jsp

--- Boersenspiel/src/controller/EndOfDay.java
+++ Boersenspiel/src/controller/EndOfDay.java
@@ -1,30 +1,28 @@
 package controller;
 
 public class EndOfDay {
-	
-	public static void endDay()
-	{
-		//TODO
+
+	public static void endDay() {
+		// TODO
 		// double rate = generateRate();
 		// Stock.setRate(rate);
 		// Stock.setCost(Stock.getCost * rate);
-		
+
 		// User.setBalance(User.getBalance - TÄGL.KOSTEN)
-		
+
 	}
-	
-	public static double generateRate()
-	{
+
+	public static double generateRate() {
 		// TODO - In die Doku:
-		//Quote zufällig generieren
-		//0-2%	zu 60%
-		//2-5%	zu 25%
-		//5-10%	zu 10%
-		//10-15% zu 5%
-		
+		// Quote zufällig generieren
+		// 0-2% zu 60%
+		// 2-5% zu 25%
+		// 5-10% zu 10%
+		// 10-15% zu 5%
+
 		int chance = (int) (Math.random() * 100);
 		double rate = 0;
-		
+
 		if (chance <= 60)
 			rate = getRandom(0, 2);
 		else if (chance > 60 && chance <= 85)
@@ -33,10 +31,10 @@ public class EndOfDay {
 			rate = getRandom(5, 10);
 		else if (chance > 95)
 			rate = getRandom(10, 15);
-		
+
 		return Math.round(rate * 100) / 100;
 	}
-	
+
 	public static double getRandom(double low, double high) {
 		return Math.random() * (high - low) + low;
 	}
